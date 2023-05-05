45011cbc99028a2b12cdb1417318cd22bceb9744
yudistrange <yudistrange@gmail.com>
2023-05-05 23:34:18 +0530

    Burnt my fingers while using the hotpot, gonna rub some aniseed on it
    Hotpot felt too basic. Expected module support in fennel (because it's a
    lisp). Didn't realize it was something that Aniseed adds.

--- init.lua
+++ init.lua
@@ -1,17 +1,11 @@
 -- ~/.config/nvim/init.lua
 
--- This init.lua file will clone hotpot into your plugins directory if
--- it is missing. Do not forget to also add hotpot to your plugin manager
--- or it may uninstall hotpot!
-
--- Consult your plugin-manager documentation for where it installs plugins.
--- packer.nvim
--- local plugin_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/hotpot.nvim'
--- paq.nvim
+-- Install path for initial plugins
 local function install_path(plugin_name) 
 	return vim.fn.stdpath('data') .. '/site/pack/packer/start/' .. plugin_name
 end
 
+-- Bootstrap plugins outside of fennel
 local function bootstrap(plugin_name, plugin_repo)
 	local plugin_path = install_path(plugin_name)
 	if vim.fn.empty(vim.fn.glob(plugin_path)) > 0 then
@@ -25,13 +19,10 @@ end
 bootstrap("packer.nvim", "https://github.com/wbthomason/packer.nvim.git")
 vim.cmd [[packadd packer.nvim]]
 
--- Install hotpot
-bootstrap("hotpot.nvim", "https://github.com/rktjmp/hotpot.nvim.git")
+-- Install Aniseed
+bootstrap("Olical/aniseed", "https://github.com/Olical/aniseed.git")
 
--- Enable fnl/ support
-require("hotpot")
-
--- Now you can load fennel code, so you could put the rest of your
--- config in a separate `~/.config/nvim/fnl/my_config.fnl` or
--- `~/.config/nvim/fnl/plugins.fnl`, etc.
-require("initialize")
+-- Set fennel init module
+vim.g["aniseed#env"] = {
+	module = "spellwork.init"
+}
