1923f53b2df57830274c3237ed6299ad510d3480
udit <yudistrange@gmail.com>
2024-06-28 23:09:18 -0400

    Fix: Keybinding for quiting a buffer

--- lua/custom/keybindings.lua
+++ lua/custom/keybindings.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape to Normal Mode" })
 vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<Esc><cmd>w<CR>", { desc = "Save" })
 vim.keymap.set({ "n", "i", "v" }, "<leader>bs", "<Esc><cmd>w<CR>", { desc = "[S]ave" })
-vim.keymap.set({ "n", "i", "v" }, "<leader>bq", "<Esc><cmd>w<CR>", { desc = "[Q]uit" })
+vim.keymap.set({ "n", "i", "v" }, "<leader>bq", "<Esc><cmd>q<CR>", { desc = "[Q]uit" })
