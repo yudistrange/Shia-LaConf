776634720128f2a404ed57bb60deda3abfd8b803
udit <yudistrange@gmail.com>
2025-10-01 11:28:26 -0400

    Disable focus

--- lua/custom/plugins/focus.lua
+++ lua/custom/plugins/focus.lua
@@ -1,40 +1,44 @@
-return {
-	"nvim-focus/focus.nvim",
-	version = false,
-	config = function()
-		local ignore_filetypes = { 'neo-tree' }
-		local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+local enable = false
 
-		local augroup =
-				vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+if enable then
+	return {
+		"nvim-focus/focus.nvim",
+		version = false,
+		config = function()
+			local ignore_filetypes = { "neo-tree" }
+			local ignore_buftypes = { "nofile", "prompt", "popup" }
 
-		vim.api.nvim_create_autocmd('WinEnter', {
-			group = augroup,
-			callback = function(_)
-				if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
-				then
-					vim.w.focus_disable = true
-				else
-					vim.w.focus_disable = false
-				end
-			end,
-			desc = 'Disable focus autoresize for BufType',
-		})
+			local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
 
-		vim.api.nvim_create_autocmd('FileType', {
-			group = augroup,
-			callback = function(_)
-				if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-					vim.b.focus_disable = true
-				else
-					vim.b.focus_disable = false
-				end
-			end,
-			desc = 'Disable focus autoresize for FileType',
-		})
+			vim.api.nvim_create_autocmd("WinEnter", {
+				group = augroup,
+				callback = function(_)
+					if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+						vim.w.focus_disable = true
+					else
+						vim.w.focus_disable = false
+					end
+				end,
+				desc = "Disable focus autoresize for BufType",
+			})
 
-		require("focus").setup({
-			excluded_filetypes = { "neo-tree" },
-		})
-	end,
-}
+			vim.api.nvim_create_autocmd("FileType", {
+				group = augroup,
+				callback = function(_)
+					if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+						vim.b.focus_disable = true
+					else
+						vim.b.focus_disable = false
+					end
+				end,
+				desc = "Disable focus autoresize for FileType",
+			})
+
+			require("focus").setup({
+				excluded_filetypes = { "neo-tree" },
+			})
+		end,
+	}
+else
+	return {}
+end
