aa167370a0be0c52f0a36b146a672937858ede3b
udit kumar <yudistrange@gmail.com>
2025-11-14 12:12:26 -0500

    Add more colorschemes

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -17,12 +17,18 @@ return {
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			-- vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
 	{ "rebelot/kanagawa.nvim" },
 	{ "catppuccin/nvim", name = "catppuccin" },
-	{ "rose-pine/neovim", name = "rose-pine" },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
 	{ "olimorris/onedarkpro.nvim" },
 	{ "marko-cerovac/material.nvim" },
 	{ "Mofiqul/dracula.nvim" },
@@ -77,4 +83,15 @@ return {
 		priority = 1000,
 	},
 	{ "miikanissi/modus-themes.nvim", priority = 1000 },
+	{
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("github-theme").setup({})
+			-- vim.cmd("colorscheme github_light")
+		end,
+	},
+	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
 }
